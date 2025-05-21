f10d42ecf8b1aef183a8b709d6294b8ad6038bed
Rohan-Panchal-Wintree <rohanpanchal@wintreetech.com>
2025-05-21 16:38:08 +0530

    Changed the testimonial image url path in public

--- src/components/Home/Testimonial.jsx
+++ src/components/Home/Testimonial.jsx
@@ -37,7 +37,7 @@ function Testimonial() {
               >
                 <div className="flex items-center mb-4">
                   <div className="overflow-hidden rounded-full w-12 h-12 bg-gray-50 border border-gray-200">
-                    <img src="src/assets/Jacob M.png" alt="Ava M." />
+                    <img src="/testimonials/Jacob M.png" alt="Ava M." />
                   </div>
                   <div className="flex-grow pl-3">
                     <h6 className="font-bold text-sm uppercase text-gray-600">
@@ -71,7 +71,7 @@ function Testimonial() {
               >
                 <div className="flex items-center mb-4">
                   <div className="overflow-hidden rounded-full w-12 h-12 bg-gray-50 border border-gray-200">
-                    <img src="src/assets/Daniel H.png" alt="Daniel H." />
+                    <img src="/testimonials/Daniel H.png" alt="Daniel H." />
                   </div>
                   <div className="flex-grow pl-3">
                     <h6 className="font-bold text-sm uppercase text-gray-600">
@@ -106,7 +106,7 @@ function Testimonial() {
               >
                 <div className="flex items-center mb-4">
                   <div className="overflow-hidden rounded-full w-12 h-12 bg-gray-50 border border-gray-200">
-                    <img src="src/assets/Emma W.png" alt="Emma W." />
+                    <img src="/testimonials/Emma W.png" alt="Emma W." />
                   </div>
                   <div className="flex-grow pl-3">
                     <h6 className="font-bold text-sm uppercase text-gray-600">
